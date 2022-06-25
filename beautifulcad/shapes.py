1e31c56089b944a5186b06451ae6b6b299605f9c
Sam Banning <samcbanning@gmail.com>
2022-06-25 00:34:56 -0400

    progress

--- beautifulcad/shapes.py
+++ beautifulcad/shapes.py
@@ -1,16 +1,27 @@
 from beautifulcad.context import Context
 
-from cadquery.occ_impl.shapes import Solid
+from cadquery.occ_impl.shapes import Solid as CQSolid
+from cadquery.occ_impl.shapes import Face as CQFace
 import jupyter_cadquery
 
 class Box:
 
     def __init__(self, l, w, h):
-        ctx = Context.current()
-        self._box = Solid.makeBox(l, w, h)
+        self.ctx = Context.current()
+        self._box = CQSolid.makeBox(l, w, h)
     
-    def faces(self, selector):
-        return self._cq.faces(selector)
+    def faces(self):
+        return [Face(f) for f in self._box.Faces()]
     
     def _ipython_display_(self):
         return jupyter_cadquery.Part(self._box).show()
+
+
+class Face:
+
+    def __init__(self, cq_face):
+        self.ctx = Context.current()
+        self._face = cq_face
+
+    def _ipython_display_(self):
+        return jupyter_cadquery.Part(self._face).show()
