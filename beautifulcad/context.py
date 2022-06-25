417df8ca28084ef6c47e866980a616c55c15951d
Samuel Banning <samuel@Samuels-iMac.local>
2022-06-25 17:12:30 -0400

    progress

--- beautifulcad/context.py
+++ beautifulcad/context.py
@@ -1,18 +1,24 @@
+import abc
+
 from cadquery import Plane
 from solids_workbench import SolidsWorkbench
 from shapes_workbench import ShapesWorkbench
 from lines_workbench import LinesWorkbench
 
 
-class Context():
+class Context(abc.ABC):
 
     def __init__(self, outer_context):
         self.outer_context = outer_context
         self.inner_context = None
 
         self.objects = []
-        if len(self.context_stack) > 0:
+        if self.outer_context is not None:
             outer_context.add(self)
+
+    @abc.abstractmethod
+    def workbench(self):
+        pass
 
     def __enter__(self):
         self.outer_context.inner_context = self
