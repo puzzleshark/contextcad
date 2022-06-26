c5b110d4fb7fb3b87019f76b8baa0979f9fdd91e
Sam Banning <samcbanning@gmail.com>
2022-06-26 15:33:15 -0400

    progress|

--- beautifulcad/shapes_workbench.py
+++ beautifulcad/shapes_workbench.py
@@ -5,13 +5,13 @@ from beautifulcad.shapes import Circle, Rect
 class ShapesWorkbench():
 
     def __init__(self, ctx):
-        self.ctx = ctx
+        self._ctx = ctx
     
     def circle(self, radius):
-        return Circle(radius, self.ctx)
+        return Circle(radius, self._ctx)
 
     def rect(self, width, height):
-        return Rect(width, height, self.ctx)
+        return Rect(width, height, self._ctx)
 
     def _ipython_display_(self):
-        return self.ctx._ipython_display_()
+        return self._ctx._ipython_display_()
--- beautifulcad/solids_workbench.py
+++ beautifulcad/solids_workbench.py
@@ -3,10 +3,10 @@ import beautifulcad.solids
 class SolidsWorkbench():
 
     def __init__(self, ctx):
-        self.ctx = ctx
+        self._ctx = ctx
 
     def box(self, length, width, height):
-            return beautifulcad.solids.Box(length, width, height, self.ctx)
+            return beautifulcad.solids.Box(length, width, height, self._ctx)
     
     def _ipython_display_(self):
-        return self.ctx._ipython_display_()
+        return self._ctx._ipython_display_()
