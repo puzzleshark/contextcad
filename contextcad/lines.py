53c0f3dad1f75b6c9be5f561aa7f1e557272e249
Samuel Banning <samcbanning@gmail.com>
2022-08-27 19:13:35 -0400

    progress

--- contextcad/lines.py
+++ contextcad/lines.py
@@ -1,20 +1,28 @@
 import cadquery as cq
+import uuid
 
 class Sketch:
     
     def __init__(self, segments, ctx):
         self._segments = segments
         self._ctx = ctx
-        self.segments = []
+        self.constraints = []
     
     def __add__(self, other):
-        return Sketch([*self.segments, other], self._ctx)
+        s = cq.Sketch()
+        for seg in self._segments:
+            s = s.segment((seg.sx, seg.sy), (seg.ex, seg.ey))
+        for seg in other._segments:
+            s = s.segment((other.sx, other.sy), (other.ex, other.ey))
+        self._ctx.current().set_for_display(s)
+        return Sketch([*self._segments, *other._segments], self._ctx)
+
 
 
     def close(self):
         pass
 
-class Line:
+class Line(Sketch):
     
     def __init__(self, sx, sy, ex, ey, ctx):
         self.sx = sx
@@ -22,17 +30,27 @@ class Line:
         self.ex = ex
         self.ey = ey
         self._ctx = ctx
+        self.constraints = []
+        self.name = uuid.uuid4()
+        super().__init__([self], ctx)
     
     def tangent(self):
         pass
+    
+    def line(self):
+        return UnDirectedLine(self.ex, self.ey, self._ctx)
+    
+    def arc(self):
+        return UnDirectedArc(self.ex, self.ey)
 
 class Arc:
-    def __init__(self, sx, sy, ex, ey, r):
+    def __init__(self, sx, sy, mx, my, ex, ey):
         self.sx = sx
         self.sy = sy
+        self.mx = mx
+        self.my = my
         self.ex = ex
         self.ey = ey
-        self.r = r
 
 
 class Point:
@@ -80,9 +98,10 @@ class DirectedArc:
 
 class UnDirectedArc:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, ctx):
         self.x = x
         self.y = y
+        self._ctx = ctx
         
-    def to(self, x, y):
+    def to(self, mx, my, ex, ey):
         return Line(self.x, self.y, x, y)
